202549db45c8730d5f9617819d331b016432f078
𝘧𝘦𝘯 <69437909+fluffeon@users.noreply.github.com>
2024-04-04 21:43:12 -0300

    E

--- periwinkle-py.py
+++ periwinkle-py.py
@@ -6,15 +6,13 @@ import platform
 
 if platform.system() == "Linux":
     pw_Manner="dbus"
-    print("dis is a linux")
-    import gi
-    gi.require_version('Gio', '2.0')
-    from gi.repository import Gio
     import dbus
+
 elif platform.system() == "Windows":
     pw_Manner="win10"
     from win10toast import ToastNotifier
     pw_Toaster=ToastNotifier()
+    
 else:
     print("You're running on an unsupported platform. Exiting.")
     exit()
@@ -25,15 +23,7 @@ pw_Client.idletimeout = None
 pw_Client.connect("localhost", 6600)
 
 def pw_Notify(title, subtitle, manner):
-    if manner=="gi":
-        Application = Gio.Application.new("hello.world", Gio.ApplicationFlags.FLAGS_NONE)
-        Application.register()
-        Notification = Gio.Notification.new(str(title))
-        Notification.set_body(str(subtitle))
-        Icon = Gio.ThemedIcon.new("dialog-information")
-        Notification.set_icon(Icon)
-        Application.send_notification(None, Notification)
-    elif manner=="dbus":
+    if manner=="dbus":
         obj = dbus.SessionBus().get_object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
         obj = dbus.Interface(obj, "org.freedesktop.Notifications")
         obj.Notify("", 0, "dialog-information", title, subtitle, [], {"urgency": 1}, 10000)
